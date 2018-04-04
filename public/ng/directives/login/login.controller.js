bd09c39f8167e1bde2e330c45e4c32435c86786c
david <david11sone@gmail.com>
2018-04-04 15:26:08 +0530

    sssssssss

--- public/ng/directives/login/login.controller.js
+++ public/ng/directives/login/login.controller.js
@@ -2,12 +2,16 @@ var app = angular.module('BTAPP');
 app.controller('loginCtrl', function ($scope, $state) {
 
 
+    
 
     $scope.login = function (username, password) {
 
         if (username != 'undefined' || password != 'undefined') {
-            $state.go("home.dashoard");
+          //  $state.go("home.dashoard");
+          angular.element(document.querySelector('[id="dim_wrapper"]')).addClass('dim');
         }
+
+
 
     }
 
